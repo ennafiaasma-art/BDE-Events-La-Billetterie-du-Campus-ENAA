650f3cf77668ccd62eaed7da169c267273149a5b
ennafiaasma-art <ennafiaasma@gmail.com>
2026-07-21 15:27:22 +0100

    la creatio de route authentificatio et autcontroller

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,15 @@
 
 use App\Http\Controllers\EvenementController;
 use Illuminate\Support\Facades\Route;
+use  App\Http\Controllers\AuthController;
 
+//les routes login
+Route::get('/login',[AuthController::class,'showLogin'])->name('login');
+Route::post('/login',[  AuthController::class,'login'])->name('login.post');
+Route::post('/login',[AuthController::class])->name('logout');
+
+
+//les routes evenement
 Route::middleware('auth')->group(function(){
     Route::resource('/admin/evenements',EvenementController::class);
 });
