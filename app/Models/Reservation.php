3f524962c7a2a8b3acbed723159b8645679a1622
ennafiaasma-art <ennafiaasma@gmail.com>
2026-07-21 16:47:36 +0100

    la creation de reservation migration et leur model et reservation controller

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -4,7 +4,32 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 class Reservation extends Model
 {
     //
+     use HasFactory;
+
+    protected $fillable = [
+        'codeReservation',
+        'dateReservation',
+        'evenement_id',
+        'etudiant_id',
+    ];
+     public function evenement()
+    {
+        return $this->belongsTo(Evenement::class);
+    }
+
+    public function etudiant()
+    {
+        return $this->belongsTo(User::class, 'etudiant_id');
+    }
+
+    public function ticket()
+    {
+        return $this->hasOne(Ticket::class);
+    }
+
 }
