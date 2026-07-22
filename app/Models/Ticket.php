d9e91730ebe0c1bcfd851b7a7537057662d31968
ennafiaasma-art <ennafiaasma@gmail.com>
2026-07-22 10:26:44 +0100

    la creation de migtration ticket et leur model avec son controller

---
+++ app/Models/Ticket.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Ticket extends Model
+{
+    protected $fillable=[
+        'code',
+        'numero',
+        'resevation_id',
+
+    ];
+    public function reservation(){
+        return $this->belongsTo(Reservation::class);
+    }
+    //
+}
